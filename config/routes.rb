f48500ebc1087e53d5f05c307294f364579cd619
Ubuntu <ubuntu@ip-172-31-47-85.ap-northeast-1.compute.internal>
2019-11-10 08:51:52 +0000

    1차 시도

---
+++ config/routes.rb
@@ -0,0 +1,17 @@
+Rails.application.routes.draw do
+  
+  devise_for :users
+  root "posts#index"
+  
+  get 'posts/index'
+  get 'posts/new'
+  
+  post 'posts/create'
+  
+  get 'posts/show'
+  get 'posts/edit'
+  get 'posts/update'
+  get 'posts/destroy'
+  
+  # For details on the DSL available within this file, see http://guides.rubyonrails.org/routing.html
+end
